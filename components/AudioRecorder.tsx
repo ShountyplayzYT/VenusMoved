414646e8f73a95750424b50b2a8053ba9242d0fd
Shaurya shah <shauryashah00@gmail.com>
2026-08-17 13:15:35 +0530

    hi

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { correctCityNames } from "./correctCityNames";
+import { correctCityNames } from "./Correctcitynames";
 
 declare global {
   interface Window {
